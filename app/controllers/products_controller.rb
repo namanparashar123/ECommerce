244957fbf61ff5c49f2fae15fb0e98174c424583
namanparashar123 <naman5770@gmail.com>
2019-11-18 11:02:49 -0600

    Modified index and shows for products and categories.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,9 @@
+class ProductsController < ApplicationController
+  def index
+    @category = Category.find(params[:category_id])
+    @products = @category.products.includes(:variants).order(:title)
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
   get 'categories/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'categories#index'
+
+  resources :categories, only: [:index] do
+    resources :products, only: [:index]
+  end
 end
