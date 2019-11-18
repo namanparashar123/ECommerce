2c862dd53d4e3e08232f6c9ddeb28c01b4a5f713
namanparashar123 <naman5770@gmail.com>
2019-11-18 10:37:05 -0600

    Adding the cart functionality.

---
+++ app/models/product.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class Product < ApplicationRecord
+  validates :title, :price, presence: true
+  has_many :product_categories
+  has_many :categories, through: :product_categories
+  has_many :variants, class_name: 'ProductVariant'
+  paginates_per 10
+
+  # mount_uploader :image, AvatarUploader
+end
