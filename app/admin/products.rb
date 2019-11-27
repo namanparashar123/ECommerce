9985e640bc78850aac9fb85c07cb0f14212775d5
namanparashar123 <naman5770@gmail.com>
2019-11-27 08:44:40 -0600

    Rubocop changes - 3.

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Product do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :title, :description, :price
+  permit_params :title, :description, :price
   #
   # or
   #
@@ -14,5 +14,5 @@ ActiveAdmin.register Product do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+
 end
