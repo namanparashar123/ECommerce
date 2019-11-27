ceb0c6ec281e4ae07e0fe603c17b4dbe75418810
namanparashar123 <naman5770@gmail.com>
2019-11-27 09:00:34 -0600

    Modified products controller.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,7 +20,7 @@ class ProductsController < ApplicationController
     if !params[:category_id].nil? && params[:category_id] != '89'
       category = Category.find(params[:category_id])
       @all_products = category.products.where('lower(title) LIKE ?', "%#{@query}%")
-                                       .order(:title).page params[:page]
+                              .order(:title).page params[:page]
     else
       @all_products = Product.where('lower(title) LIKE ?', "%#{@query}%")
                              .order(:title).page params[:page]
